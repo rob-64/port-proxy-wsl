d6f4bbbecee5e90ef7f4714aad1d8bcce0794a9d
Rob <2374685+rob-64@users.noreply.github.com>
2025-10-16 08:19:21 -0400

    Refactor port proxy functions for improved logging

--- port_proxy_wsl.ps1
+++ port_proxy_wsl.ps1
@@ -26,18 +26,45 @@ function Reset-State {
 
 Reset-State
 
-function Delete-Port-Proxy($port) {
-    Log-Verbose "removing port proxy $port"
+function Delete-Port-Proxy {
+    param (
+        [int]$port,
+        [bool]$log = $true
+    )
+    if($log) {
+        Log-Verbose "removing port proxy $port"
+    }
     Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port" | out-null
     Invoke-Expression "netsh advfirewall firewall del rule name='WSL Port Proxy' protocol=TCP localport=$port" | out-null
 }
 
-function Add-Port-Proxy($port, $dstAddr, $dstPort){
-    Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port" | out-null
-    Invoke-Expression "netsh advfirewall firewall del rule name='WSL Port Proxy' protocol=TCP localport=$port" | out-null
+function Add-Port-Proxy($port, $dstAddr, $dstPort) {
+    Delete-Port-Proxy $port $false
     Log-Verbose "adding port proxy: $port -> $dstPort"
     Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port connectaddress=$dstAddr connectport=$dstPort" | out-null
     Invoke-Expression "netsh advfirewall firewall add rule name='WSL Port Proxy' dir=in action=allow protocol=TCP localport=$port" | out-null
+}
+
+function Add-Ports {
+    param (
+        [int[]]$ports,
+        [string]$cmd,
+        [bool]$log = $true
+    )
+    if($log) {
+        Log-Verbose "checking for ports with $cmd"
+    }
+    $cmdPorts = wsl -u root -e sh -c $cmd
+    Log-Verbose "ports from command: $cmdPorts"
+    Log-Verbose "current ports: $ports"
+    foreach ($port in $cmdPorts) {
+        $portNumber = [int]::Parse($port)
+        if ($portNumber -ge 22 -and $portNumber -le 65535 -and !$ports.Contains($portNumber)) {
+            $ports += $portNumber
+        }
+    }
+    Log-Verbose "ports after add-ports: $ports"
+    return $ports
 }
 
 while ($true) {
@@ -54,13 +81,16 @@ while ($true) {
         }
         if ($wslEth0.Length -ne 0) {
             $newPorts = @()
-            $localPorts = wsl -u root -e sh -c "ss -ltn | tail -n +2 | awk '{print `$4}' | awk -F':' '{print `$NF}' | sort -n | uniq"
-            # Log-Verbose $localPorts
-            foreach ($port in $localPorts) {
-                if ($port -ge 22 -and $port -le 65535 -and !$newPorts.Contains($port)) {
-                    $newPorts += $port
-                }
+            # find any bound ports with ss
+            $newPorts = Add-Ports $newPorts "ss -ltn | tail -n +2 | awk '{print `$4}' | awk -F':' '{print `$NF}' | sort -n | uniq"
+            Log-Verbose $newPorts
+            if(!$newPorts.Contains(443)) {
+                $newPorts += 443
             }
+            if(!$newPorts.Contains(80)) {
+                $newPorts += 80
+            }
+            Log-Verbose $newPorts
             $updatedPorts = $false
             # sometimes we get an empty array
             if ($newPorts.Length -gt 0 -Or $newPorts.Length -ne $currentPorts.Length) {
@@ -70,9 +100,9 @@ while ($true) {
                     if (!$newPorts.Contains($currentPort)) {
                         $listenPort = $currentPort
                         # always forward 443 -> 8443
-                        if ($listenPort -eq 8443) {
-                            $listenPort = 443
-                        }
+                        # if ($listenPort -eq 8443) {
+                        #     $listenPort = 443
+                        # }
                         Delete-Port-Proxy $listenPort
                         $updatedPorts = $true
                     }
@@ -82,13 +112,9 @@ while ($true) {
                     $newPort = $newPorts[$i]
                     $listenPort = $newPort
                     # always forward 443 -> 8443
-                    if ($listenPort -eq 8443) {
-                        $listenPort = 443
-                    }
-                    # always forward 80 -> 8000
-                    if ($listenPort -eq 8000) {
-                        $listenPort = 80
-                    }
+                    # if ($listenPort -eq 8443) {
+                    #     $listenPort = 443
+                    # }
                     if (!$currentPorts.Contains($newPort)) {
                         Add-Port-Proxy $listenPort $wslEth0 $newPort
                         $updatedPorts = $true
@@ -99,8 +125,9 @@ while ($true) {
                     $currentPorts = $newPorts
                 }
             }
-            # Clear-Host
-            # Invoke-Expression "netsh interface portproxy show v4tov4"
+            
+            Clear-Host
+            Invoke-Expression "netsh interface portproxy show v4tov4"
         }
         else {
             Log-Verbose "WSL IP is empty..."
